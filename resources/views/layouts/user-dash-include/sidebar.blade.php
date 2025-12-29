2a9e579f85ecc041c927415a1c992ca793111f93
Snehasish Bhurisrestha <snehasish7031@gmail.com>
2025-12-30 00:34:57 +0530

    29-12-2025

--- resources/views/layouts/user-dash-include/sidebar.blade.php
+++ resources/views/layouts/user-dash-include/sidebar.blade.php
@@ -55,7 +55,7 @@
     @endforeach
 
     <!-- Divider -->
-    <hr class="sidebar-divider">
+    <hr class="sidebar-divider my-1">
 
     <!-- Nav Item - Dashboard -->
     <li class="nav-item">
@@ -65,7 +65,7 @@
     </li>
 
     <!-- Divider -->
-    <hr class="sidebar-divider">
+    <hr class="sidebar-divider my-1">
 
     <li class="nav-item">
         <a class="nav-link" href="{{ route('community') }}">
@@ -74,7 +74,7 @@
     </li>
 
     <!-- Divider -->
-    <hr class="sidebar-divider">
+    <hr class="sidebar-divider my-1">
 
     <!-- Heading -->
     <div class="sidebar-heading">
@@ -86,6 +86,8 @@
             <i class="fas fa-wallet"></i>
             <span>Transaction</span></a>
     </li>
+    <!-- Divider -->
+    <hr class="sidebar-divider my-1">
 
     <!-- Withdrawals -->
     <li class="nav-item {{ request()->routeIs('user.withdrawals') ? 'active' : '' }}">
@@ -94,6 +96,8 @@
             <span>Withdrawals</span>
         </a>
     </li>
+    <!-- Divider -->
+    <hr class="sidebar-divider my-1">
 
     <!-- Payment Account -->
     <li class="nav-item {{ request()->routeIs('user.payment.account') ? 'active' : '' }}">
@@ -103,37 +107,8 @@
         </a>
     </li>
 
-    <!-- Nav Item - Pages Collapse Menu -->
-    {{-- <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
-            aria-expanded="true" aria-controls="collapsePages">
-            <i class="fas fa-fw fa-folder"></i>
-            <span>Pages</span>
-        </a>
-        <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Login Screens:</h6>
-                <a class="collapse-item" href="login.html">Login</a>
-                <a class="collapse-item" href="register.html">Register</a>
-                <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                <div class="collapse-divider"></div>
-                <h6 class="collapse-header">Other Pages:</h6>
-                <a class="collapse-item" href="404.html">404 Page</a>
-                <a class="collapse-item" href="blank.html">Blank Page</a>
-            </div>
-        </div>
-    </li>
-
-    <!-- Nav Item - Charts -->
-    <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-            <i class="fas fa-fw fa-chart-area"></i>
-            <span>Charts</span></a>
-    </li>
-     --}}
-
     <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
+    <hr class="sidebar-divider my-1 d-none d-md-block">
 
     <!-- Sidebar Toggler (Sidebar) -->
     <div class="text-center d-none d-md-inline">
